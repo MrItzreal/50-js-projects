6cd8f82b99a971661eb5ac2d20d4f043bad35a66
Israel <israel4321@hotmail.com>
2024-02-28 12:27:08 -0500

    added notes explaining what each line of code does.

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -3,8 +3,12 @@ const prev = document.getElementById("prev");
 const next = document.getElementById("next");
 const circles = document.querySelectorAll(".circle");
 
+//this is our index that will go as
+//far as 4 and not lower than 1.
 let currentActive = 1;
 
+//This makes sure that we don't go above
+// 4 when we click next btn since we only have 4 circles.
 next.addEventListener("click", () => {
   currentActive++;
 
@@ -14,6 +18,8 @@ next.addEventListener("click", () => {
   update();
 });
 
+//This makes sure that we don't go below
+// 1 when we click prev to avoid going negative.
 prev.addEventListener("click", () => {
   currentActive--;
 
@@ -23,6 +29,8 @@ prev.addEventListener("click", () => {
   update();
 });
 
+//This function updates the DOM whether
+//we click "prev" or click "next" buttons.
 function update() {
   circles.forEach((circle, i) => {
     if (i < currentActive) {
