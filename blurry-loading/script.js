af0361e7a500a666b5eb58f6a38fc717b452c1b8
MrItzreal <is.wsie@gmail.com>
2024-03-01 09:55:06 -0500

    W/ JavaScript we mapped a range of numbers to another range of numbers by using a function. Which gave the blur/opacity effects on the page.

--- blurry-loading/script.js
+++ blurry-loading/script.js
@@ -6,9 +6,20 @@ let int = setInterval(blurring, 30);
 
 function blurring() {
   load++;
+
   if (load > 99) {
     clearInterval(int);
   }
 
-  console.log(load);
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  // the code below uses a template string to scale the blur.
+  // so bg is the var, .style is the css style sheet and .filter
+  // is the method. we used the dots to access and insert scale values.
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (number, inMin, inMax, outMin, outMax) => {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+};
