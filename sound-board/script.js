766a74549b852bdf0270487e1fd2043f442c35f7
MrItzreal <is.wsie@gmail.com>
2024-03-05 17:02:38 -0500

    added explanations of the code on the JS file.

--- sound-board/script.js
+++ sound-board/script.js
@@ -1,5 +1,12 @@
 const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
 
+// 1:loop sounds
+// 2:create a button element
+// 3:add class to new button
+// 4:insert the sounds text
+// 5:get ID and append the child
+// 6:make sure sounds play one at a time.
+
 sounds.forEach((sound) => {
   const btn = document.createElement("button");
   btn.classList.add("btn");
