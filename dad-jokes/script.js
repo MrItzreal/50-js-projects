6e652eaff18441af61363913ac1762600a0e6269
MrItzreal <is.wsie@gmail.com>
2024-03-06 09:46:57 -0500

    Learned the difference between .then() and Async/Await.

---
+++ dad-jokes/script.js
@@ -0,0 +1,39 @@
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+jokeBtn.addEventListener("click", generateJoke);
+
+generateJoke();
+
+//USING ASYNC/AWAIT
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const res = await fetch("https://icanhazdadjoke.com", config);
+
+  const data = await res.json();
+
+  jokeEl.innerHTML = data.joke;
+}
+
+//USING .then()
+//I could have headers/Accept etc.. within the
+//fetch but creating a variable makes it cleaner.
+
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   };
+
+//   fetch("https://icanhazdadjoke.com", config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke;
+//     });
+// }
