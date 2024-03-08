87a714dae2b51a7cc890c3347efd3bdd48f0b72a
MrItzreal <is.wsie@gmail.com>
2024-03-08 14:43:18 -0500

    added an event listener so when i click a cup it becomes full.

---
+++ 16-drink-water/script.js
@@ -0,0 +1,20 @@
+const smallCups = document.querySelectorAll(".cup-small");
+const liters = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
+
+smallCups.forEach((cup, i) => {
+  cup.addEventListener("click", () => {
+    highlightCups(i);
+  });
+});
+
+function highlightCups(i) {
+  smallCups.forEach((cup, i2) => {
+    if (i2 <= i) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
+}
