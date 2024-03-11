a6069d59a20cc6f4a470dfa2940de37943edb6c6
MrItzreal <is.wsie@gmail.com>
2024-03-11 14:44:17 -0400

    boiler plate for the api request

---
+++ 17-movie-app/script.js
@@ -0,0 +1,15 @@
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b2b267baf2bf186c5a943686982d360b&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_URL =
+  'https://api.themoviedb.org/3/search/movie?api_key=b2b267baf2bf186c5a943686982d360b&query="'
+
+
+
+
+  
+//https://www.themoviedb.org/API
+//I added my api key and a &page=1 stating that I only need 1 page.
+//w means width so w1280 is the width of the photo
+//it could have been any number.
+//do not add / after number since the image source has one already.
