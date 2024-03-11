615e887373ae07c3c844e8d1dea6634b03a06c73
MrItzreal <is.wsie@gmail.com>
2024-03-11 09:21:31 -0400

    added the highlight/unhighlight functionality to the small cups

--- 16-drink-water/script.js
+++ 16-drink-water/script.js
@@ -10,6 +10,16 @@ smallCups.forEach((cup, idx) => {
 });
 
 function highlightCups(idx) {
+  //check if next cup is full if so if you click
+  //the full cup again it will become empty.
+  if (
+    smallCups[idx].classList.contains("full") &&
+    !smallCups[idx].nextElementSibling.classList.contains("full")
+  ) {
+    idx--;
+  }
+
+  //add the full class thus highlighting the cup.
   smallCups.forEach((cup, idx2) => {
     if (idx2 <= idx) {
       cup.classList.add("full");
