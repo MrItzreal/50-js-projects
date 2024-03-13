11b7a29484ec1de2991981ae6102f271381468e7
MrItzreal <is.wsie@gmail.com>
2024-03-13 11:30:40 -0400

    added JS to add the movement of the clock and added the time and dates.

--- 19-theme-clock/script.js
+++ 19-theme-clock/script.js
@@ -41,3 +41,64 @@ toggle.addEventListener("click", (e) => {
     e.target.innerHTML = "Light Mode";
   }
 });
+
+function setTime() {
+  const time = new Date();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const date = time.getDate();
+  const hours = time.getHours();
+  const hoursForClock = hours % 12; //this is for 12 hours instead of 24.
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const ampm = hours >= 12 ? "PM" : "AM";
+
+  //Remember we doing a 12 hour format. O is the inMin thus why 11 is the inMax.
+  //0 is basically a 1 so 1 + 11 = 12.Hope my future self understands this note.
+  //360 is the rotation of the clock hands.
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hoursForClock,
+    0,
+    11,
+    0,
+    360
+  )}deg)`;
+
+  //60 minutes in an hour inMin is 0 and inMax 59.
+  //0 is basically a 1 so 1 + 59 = 60.
+  //360 is the rotation of the clock hands.
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  //60 seconds in each minute so inMin is 0 and inMax 59.
+  //0 is basically a 1 so 1 + 59 = 60.
+  //360 is the rotation of the clock hands.
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  // if minute less than 10 then add a zero.
+  // We want 12:05 NOT 12:5 thus why ternary operator.
+  timeEl.innerHTML = `${hoursForClock}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${ampm}`;
+
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
+}
+
+//map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (number, inMin, inMax, outMin, outMax) => {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+};
+
+setTime();
+setInterval(setTime, 1000);
