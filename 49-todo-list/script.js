82b84e01b73df092c63e3aafda290ce035b2cea4
MrItzreal <is.wsie@gmail.com>
2024-04-04 17:35:32 -0400

    added localstorage

--- 49-todo-list/script.js
+++ 49-todo-list/script.js
@@ -2,6 +2,13 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUl = document.getElementById("todos");
 
+//LocalStorage
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault(); //prevents submit
 
@@ -25,14 +32,45 @@ function addTodo(todo) {
 
     todoEl.addEventListener("click", () => {
       todoEl.classList.toggle("completed");
+
+      updateLS();
     });
 
     todoEl.addEventListener("contextmenu", (e) => {
       e.preventDefault(); //prevents right click menu popup
       todoEl.remove();
+      updateLS();
     });
 
     todosUl.appendChild(todoEl);
     input.value = "";
+
+    updateLS();
   }
 }
+
+function updateLS() {
+  todosEl = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todosEl.forEach((todoEl) =>
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    })
+  );
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+//localStorage: data is saved even after closing window,
+//it only deletes when you explicitly clear it yourself.
+
+//SessionStorage: data is saved until you close window,
+//once closed the data is cleared.
+
+//Local storage uses the key & value pairs:
+// Examples:
+// localStorage.setItem("name","Izzy");
+// localStorage.getItem("name");
+// localStorage.removeItem("name");
